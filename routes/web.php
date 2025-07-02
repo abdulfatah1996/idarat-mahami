61fc651e376bb3a0362131c5f6121daa3f4aaa9c
Abdelfatah Obeid <abdro1996@gmail.com>
2025-07-02 10:11:39 +0300

    Update README: switched to account registration instead of demo credentials

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use App\Livewire\Ui\Notifications;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 
